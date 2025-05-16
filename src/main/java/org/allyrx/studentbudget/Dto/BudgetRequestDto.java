2b4a5b05152607e8c3f87a6818235c1b75c4d930
allyRx <herindrainyall2003@gmail.com>
2025-05-16 17:57:53 +0300

    features:  delete budget

--- src/main/java/org/allyrx/studentbudget/Dto/BudgetRequestDto.java
+++ src/main/java/org/allyrx/studentbudget/Dto/BudgetRequestDto.java
@@ -6,9 +6,8 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class BudgetRequestDto {
--- src/main/java/org/allyrx/studentbudget/Entites/Budget.java
+++ src/main/java/org/allyrx/studentbudget/Entites/Budget.java
@@ -4,9 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 
 @Entity
